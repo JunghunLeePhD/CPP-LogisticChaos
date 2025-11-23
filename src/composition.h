6f3f6945d6568031834955511a7c336e3f64f8a8
JunghunLeePhD <yijeonghunxd@gmail.com>
2025-11-23 08:43:14 +0000

    Update (`composition.h`): add `identity_func`, `compose`, `pow_func` functions

--- src/composition.h
+++ src/composition.h
@@ -1,9 +1,18 @@
 #ifndef COMPOSITION_H
 #define COMPOSITION_H
 
+<<<<<<< HEAD
 #include <functional>
 
 extern std::function<double(double)> identity_func;
+=======
+#include <iostream>
+#include <functional>
+
+auto identity_func = [](double x) { 
+    return x; 
+};
+>>>>>>> de870e0 (Update (`composition.h`): add `identity_func`, `compose`, `pow_func` functions)
 
 template<typename F, typename G>
 std::function<double(double)> compose(F f, G g) {
@@ -12,6 +21,21 @@ std::function<double(double)> compose(F f, G g) {
     };
 }
 
+<<<<<<< HEAD
 std::function<double(double)> power_func(std::function<double(double)> f, int n);
+=======
+std::function<double(double)> power_func(std::function<double(double)> f, int n) {
+    if (n <= 0) {
+        return identity_func; 
+    }
+    if (n % 2 == 0) {
+        auto g = power_func(f, n / 2);
+        return compose(g, g);
+    } else {
+        auto g = power_func(f, n - 1);
+        return compose(f, g);
+    }
+}
+>>>>>>> de870e0 (Update (`composition.h`): add `identity_func`, `compose`, `pow_func` functions)
 
 #endif
