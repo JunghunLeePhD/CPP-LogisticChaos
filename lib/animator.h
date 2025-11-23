db02da9ff63f18573749676a90a1b61810e0ef27
JunghunLeePhD <yijeonghunxd@gmail.com>
2025-11-23 14:44:32 +0000

    Update (`animator.h/cpp`): add `run_strip_sweep` function

--- lib/animator.h
+++ lib/animator.h
@@ -36,7 +36,9 @@ public:
     OrbitAnimator& set_function_color(const Color& c);
     OrbitAnimator& set_web_colors(const Color& start, const Color& end);
 
-    void run_sweep(double r_start, double r_end, int total_frames, const std::string& output_folder);
+    void run_cobweb_sweep(double r_start, double r_end, int total_frames, const std::string& output_folder);
+
+    void run_strip_sweep(double r_start, double r_end, int total_frames, const std::string& output_folder);
 };
 
 #endif
