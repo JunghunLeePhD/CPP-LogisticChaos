7fcdaeb9ce4565ce93ba15bdb39742eeb1d3658c
JunghunLeePhD <yijeonghunxd@gmail.com>
2025-11-24 06:34:47 +0000

    Refactor (`composition.cpp`): use 'smart pointers' and 'STL' algorithms

--- lib/composition.cpp
+++ lib/composition.cpp
@@ -1,5 +1,7 @@
 #include "composition.h"
-#include <algorithm>
+#include <algorithm> 
+#include <iterator>  
+#include <vector>
 
 std::function<double(double)> identity_func = [](double x) { 
     return x; 
@@ -7,28 +9,26 @@ std::function<double(double)> identity_func = [](double x) {
 
 std::function<double(double)> power_func(std::function<double(double)> f, int n) {
     if (n <= 0) {
-        return identity_func; 
+        return identity_func;
     }
-    if (n % 2 == 0) {
-        auto g = power_func(f, n / 2);
-        return compose(g, g);
-    } else {
-        auto g = power_func(f, n - 1);
-        return compose(f, g);
-    }
+
+    return [f, n](double x) {
+        double result = x;
+        for (int i = 0; i < n; ++i) {
+            result = f(result);
+        }
+        return result;
+    };
 }
 
 std::vector<double> get_orbit(std::function<double(double)> f, double x0, int iterations, int skip_transient) {
     std::vector<double> orbit(iterations + 1);
 
-    double start_x;
-    auto jump_func = power_func(f, skip_transient);
-    start_x = jump_func(x0);
+    double current_x = power_func(f, skip_transient)(x0);
 
-    orbit[0] = start_x;
-    double current_x = start_x;
+    orbit[0] = current_x;
 
-    std::generate(std::next(orbit.begin()), orbit.end(), [&]() mutable {
+    std::generate(std::next(orbit.begin()), orbit.end(), [&current_x, &f]() {
         current_x = f(current_x);
         return current_x;
     });
